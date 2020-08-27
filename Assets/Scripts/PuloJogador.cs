8798dfedd5b6713a575d75dba035e33fd8c271b5
Paulo Salvatore <paulo@salvatore.tech>
2020-08-26 21:42:38 -0300

    Implementação do pulo do jogador, com checagem se o jogador está no chão ou não

---
+++ Assets/Scripts/PuloJogador.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuloJogador : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    public float forcaPulo = 50f;
+
+    bool estaNoChao;
+
+    public float distancia = 1;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && estaNoChao)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+            rb.AddForce(Vector2.up * forcaPulo);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        estaNoChao = Physics2D.Raycast(transform.position, Vector2.down, distancia, 1 << LayerMask.NameToLayer("Chão"));
+
+        Debug.DrawRay(transform.position, Vector2.down * distancia, estaNoChao ? Color.green : Color.red);
+    }
+}
