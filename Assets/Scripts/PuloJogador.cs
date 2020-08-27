9baebdca7ebed8debd12fbdac54ce8d3a99fe228
Paulo Salvatore <paulo@salvatore.tech>
2020-08-27 19:37:57 -0300

    Pulo duplo e melhorias na detecção do chão com 2 raycasts

--- Assets/Scripts/PuloJogador.cs
+++ Assets/Scripts/PuloJogador.cs
@@ -2,29 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class PuloJogador : MonoBehaviour
 {
+    [Header("Componentes")]
     public Rigidbody2D rb;
 
+    [Header("Configurações")]
     public float forcaPulo = 50f;
+
+    bool puloDuploDisponivel = true;
+
+    [Header("Detectores de Chão")]
+    public float distancia = 1;
 
     bool estaNoChao;
 
-    public float distancia = 1;
+    public Transform detectorChao1;
+
+    public Transform detectorChao2;
 
     void Update()
     {
-        if (Input.GetButtonDown("Jump") && estaNoChao)
+        if (Input.GetButtonDown("Jump") && puloDuploDisponivel)
         {
+            if (!estaNoChao)
+            {
+                puloDuploDisponivel = false;
+            }
+
             rb.velocity = new Vector2(rb.velocity.x, 0);
             rb.AddForce(Vector2.up * forcaPulo);
         }
+
+        Debug.DrawRay(detectorChao1.position, Vector2.down * distancia, estaNoChao ? Color.green : Color.red);
+        Debug.DrawRay(detectorChao2.position, Vector2.down * distancia, estaNoChao ? Color.green : Color.red);
     }
 
     private void FixedUpdate()
     {
-        estaNoChao = Physics2D.Raycast(transform.position, Vector2.down, distancia, 1 << LayerMask.NameToLayer("Chão"));
+        estaNoChao = Physics2D.Raycast(detectorChao1.position, Vector2.down, distancia, 1 << LayerMask.NameToLayer("Chão"))
+            || Physics2D.Raycast(detectorChao2.position, Vector2.down, distancia, 1 << LayerMask.NameToLayer("Chão"));
 
-        Debug.DrawRay(transform.position, Vector2.down * distancia, estaNoChao ? Color.green : Color.red);
+        if (estaNoChao && !puloDuploDisponivel)
+        {
+            puloDuploDisponivel = true;
+        }
     }
 }
