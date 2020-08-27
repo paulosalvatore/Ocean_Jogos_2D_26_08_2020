2a32f137ab2991e85719e7ff2d630a0eb6616105
Paulo Salvatore <paulo@salvatore.tech>
2020-08-27 20:57:22 -0300

    Tilemap e Parallax

---
+++ Assets/Scripts/MovimentoBackground.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoBackground : MonoBehaviour
+{
+    public Transform jogador;
+
+    public float velocidade = 1f;
+
+    private void Update()
+    {
+        transform.position = jogador.position * velocidade;
+    }
+}
---
+++ Assets/Scripts/MovimentoCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoCamera : MonoBehaviour
+{
+    public Transform jogador;
+
+    private void Update()
+    {
+        transform.position = new Vector3(
+            jogador.position.x,
+            jogador.position.y,
+            transform.position.z
+        );
+    }
+}
