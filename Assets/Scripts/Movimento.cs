0ed8ec03eb892737926cba3b0b13027ccf8a8da2
Paulo Salvatore <paulo@salvatore.tech>
2020-08-27 21:14:52 -0300

    Inimigo 'serra' e script de movimento customizável

---
+++ Assets/Scripts/Movimento.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimento : MonoBehaviour
+{
+    public float velocidade = 1f;
+
+    public Vector2 direcao = Vector2.right;
+
+    public bool habilitarInversaoSentido;
+
+    public float tempoInverter = 1f;
+
+    int sentidoAtual = 1;
+
+    private void Start()
+    {
+        if (habilitarInversaoSentido)
+        {
+            InvokeRepeating("InverterSentido", tempoInverter, tempoInverter);
+        }
+    }
+
+    void InverterSentido()
+    {
+        sentidoAtual *= -1;
+    }
+
+    private void Update()
+    {
+        transform.Translate(direcao * velocidade * sentidoAtual * Time.deltaTime);
+    }
+}
